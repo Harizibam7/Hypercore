926042d9eb569d592de2897af05f70240b7d8d39
Harizibam7 <harizibamv@gmail.com>
2025-01-06 09:45:03 +0530

    Updated router for agency and subaccount

---
+++ src/app/(main)/agency/page.tsx
@@ -0,0 +1,17 @@
+import { getAuthUserDetails } from "@/lib/queries";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+
+const Page = async () => {
+    const authUser = await currentUser()
+    if(!authUser) return redirect('/sign-in')
+
+    const agencyId = await verifyAndAcceptInvitation();
+
+    //get user details    
+    const user = await getAuthUserDetails();
+    return <div>Agency</div>
+
+}
+export default Page
